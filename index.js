0e7c30e02dd9aaad3312c6822e184541d96235fe
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-01-20 14:03:18 -0500

    finished mvp on index.js

--- index.js
+++ index.js
@@ -169,8 +169,22 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian {
+   constructor(atts) {
+     super(atts);
+     this.previousBackground = atts.previousBackground;
+     this.className = atts.className;
+     this.favSubjects = atts.favSubjects;
+   }
+  listSubjects() {
+    return `Loving ${this.favSubjects[0]}, ${this.favSubjects[1]}, ${this.favSubjects[2]}!`
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun spring challenge on ${subject}`
+  }
  }
   
   /*
@@ -186,9 +200,20 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(atts) {
+        super(atts);
+        this.gradClassName = atts.gradClassName;
+        this.favInstructor = atts.favInstructor;
+     }
+     standUp(slack) {
+       return `${this.name} announces to ${slack}, @channel standy times!`
+     }
+     debugsCode(student, subject) {
+       return `${this.name} debugs ${student.name}'s code on ${subject}`
+     }
  }
+
   /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
